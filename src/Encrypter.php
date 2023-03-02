4a81827d1d008d418b92bdbfb7267c2a5d367a3f
Björn Büttner <idrinth@users.noreply.github.com>
2023-03-02 18:57:18 +0100

    fix encryption hopefully

--- src/Encrypter.php
+++ src/Encrypter.php
@@ -2,23 +2,22 @@
 
 namespace De\Idrinth\Duplication;
 
-use phpseclib3\Crypt\RSA;
-use phpseclib3\Crypt\RSA\PublicKey;
+use phpseclib3\Crypt\AES;
 
 class Encrypter
 {
-    private ?PublicKey $key = null;
+    private AES $key;
 
     public function __construct()
     {
-        $file = dirname(__DIR__) . '/public.key';
-        if (is_file($file) && is_readable($file)) {
-            $this->key = RSA::loadPublicKey(file_get_contents($file));
-        }
+        $this->key = new AES('ctr');
+        $this->key->setIV($_ENV['ENCRYPTION_AES_IV']);
+        $this->key->setKeyLength(intval($_ENV['ENCRYPTION_AES_LENGTH'], 10));
+        $this->key->setKey($_ENV['ENCRYPTION_AES_IV']);
     }
     public function encrypt(string $data, bool $encrypt): string
     {
-        if (!$encrypt || !$this->key) {
+        if (!$encrypt) {
             return $data;
         }
         return $this->key->encrypt($data) ?: '';

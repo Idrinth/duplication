61804e164f24f1b7d3f03c39ce728655c24b711a
Björn Büttner <idrinth@users.noreply.github.com>
2023-03-02 18:59:28 +0100

    re-add dotenv

--- duplicate.php
+++ duplicate.php
@@ -1,9 +1,12 @@
 <?php
 
 use De\Idrinth\Duplication\Command;
+use Dotenv\Dotenv;
 
 require_once ('vendor/autoload.php');
 
 ini_set('memory_limit', -1);
+
+Dotenv::createImmutable(__DIR__)->load();
 
 (new Command())->run();
